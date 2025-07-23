88581ba5479c8458910a6270340e999aaaf5cbe2
Master Toad <101490896+MasterToad7@users.noreply.github.com>
2025-07-23 14:51:18 -0400

    Add files via upload

---
+++ csv_skim.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_TOKENS 16384
+#define MAX_TOKEN_LENGTH 32
+
+typedef struct
+{
+    char token[MAX_TOKEN_LENGTH];
+    int count;
+} TokenCount;
+
+int main()
+{
+    char* input[256];
+    printf("Enter CSV Victim: ");
+    scanf("%s", &input);
+
+    FILE *fp;
+    char row[255];
+    fp = fopen(input, "r");
+
+    FILE *fp2;
+    fp2 = fopen("csvskimlog.csv", "w");
+
+    if (fp == NULL)
+    {
+        printf("Error Reading File\n");
+        return -1;
+    }
+    TokenCount tokens[MAX_TOKENS];
+    int tokenIndex = 0;
+
+    while (feof(fp) != 1)
+    {
+
+        fgets(row, 255, fp);
+        
+        row[strcspn(row, "\n")] = '\0';
+
+        char *token = strtok(row, " ,");
+        while (token != NULL)
+        {
+            int found = 0;
+            for (int i = 0; i < tokenIndex; i++)
+            {
+                if (strcmp(tokens[i].token, token) == 0)
+                {
+                    tokens[i].count++;
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found && tokenIndex < MAX_TOKENS)
+            {
+                strncpy(tokens[tokenIndex].token, token, MAX_TOKEN_LENGTH - 1);
+                tokens[tokenIndex].token[MAX_TOKEN_LENGTH - 1] = '\0';
+                tokens[tokenIndex].count = 1;
+                tokenIndex++;
+            }
+            token = strtok(NULL, " ,");
+        }
+    }
+    fprintf(fp2, "Token,Count\n");
+    for (int i = 0; i < tokenIndex; i++)
+    {
+        fprintf(fp2, "%s,%d\n", tokens[i].token, tokens[i].count);
+    }
+
+    fclose(fp);
+    fclose(fp2);
+
+    return 0;
+}
